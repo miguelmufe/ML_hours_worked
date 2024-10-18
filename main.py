da391a23658dd2df90a980d1c9914ae8566b5398
miguelmufe <138031494+miguelmufe@users.noreply.github.com>
2024-10-18 11:31:11 +0200

    uploaded data and created main file

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+
+
+# Importing the dataset
